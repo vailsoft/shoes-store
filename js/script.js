6d1f2303a9536387a87a737b0f3978a15b1be02b
vailsoft <vailsoft@hotmail.com>
2021-08-18 23:10:26 -0300

    Terminada a segunda página e finalizado

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.custom-radio')[1].style.cssText = "margin-left: 80px"
+document.querySelectorAll('.custom-radio')[2].style.cssText = "margin-left: 80px"
+
+document.querySelectorAll('.custom_checkbox')[1].style.cssText = "margin-left: 10px"
+document.querySelectorAll('.custom_checkbox')[2].style.cssText = "margin-left: 10px"
